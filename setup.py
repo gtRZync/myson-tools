67fc3cf68610f768b66a83eacf210ae18f360f3c
Myson Dio <sendorfbg@gmail.com>
2026-01-06 20:06:59 -0300

    versioning: version bump to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="myson-tools",
-    version="0.1.2",
+    version="0.1.4",
     description="A set of bioinformatics tools (by Myson)",
     author="Myson Dio",
     license='GPL-3.0',
